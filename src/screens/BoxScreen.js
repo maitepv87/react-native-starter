11e852e992d5243309a35c0cb4f7e3a54649708a
Maite Perez Vento <maitepv87@gmail.com>
2025-11-01 15:19:28 -0400

    handle Screen Layoyt

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,21 +3,39 @@ import { Text, StyleSheet, View } from "react-native";
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+    <View style={styles.parentStyle}>
+      <View style={styles.viewOneStyle} />
+      <View style={styles.viewTwoStyle} />
+      <View style={styles.viewThreeStyle} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  viewStyle: {
+  parentStyle: {
     borderWidth: 3,
     borderColor: "black",
+    height: 200,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  textStyle: {
-    borderWidth: 10,
-    borderColor: "red",
-    margin: 20,
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "red",
+  },
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "green",
+    // marginTop: 50,
+    // top: 50,
+    alignSelf: "flex-end",
+  },
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "purple",
   },
 });
 
