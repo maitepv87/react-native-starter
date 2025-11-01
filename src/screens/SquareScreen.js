71e8ae12fc642afa562d0bb37dca012920ec6e57
Maite Perez Vento <maitepv87@gmail.com>
2025-11-01 13:22:30 -0400

    refactor to use useReducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -4,6 +4,12 @@ import ColorCounter from "../components/ColorCounter";
 
 const COLOR_INCCREMENT = 15;
 
+const ACTION_TYPES = {
+  CHANGE_RED: "CHANGE_RED",
+  CHANGE_GREEN: "CHANGE_GREEN",
+  CHANGE_BLUE: "CHANGE_BLUE",
+};
+
 const initialState = {
   red: 0,
   green: 0,
@@ -11,48 +17,72 @@ const initialState = {
 };
 
 const reducer = (state, action) => {
-  switch (action.colorToChange) {
-    case "red":
-      return { ...state, red: state.red + action.amount };
-    case "green":
-      return { ...state, green: state.green + action.amount };
-    case "blue":
-      return { ...state, blue: state.blue + action.amount };
+  switch (action.type) {
+    case ACTION_TYPES.CHANGE_RED:
+      return state.red + action.payload > 255 || state.red + action.payload < 0
+        ? state
+        : { ...state, red: state.red + action.payload };
+    case ACTION_TYPES.CHANGE_GREEN:
+      return state.green + action.payload > 255 ||
+        state.green + action.payload < 0
+        ? state
+        : { ...state, green: state.green + action.payload };
+    case ACTION_TYPES.CHANGE_BLUE:
+      return state.blue + action.payload > 255 ||
+        state.blue + action.payload < 0
+        ? state
+        : { ...state, blue: state.blue + action.payload };
     default:
-      return { ...state };
+      return state;
   }
 };
 
 const SquareScreen = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { red, green, blue } = state;
+
   return (
     <View>
       <ColorCounter
         color="Red"
         onIncrease={() =>
-          dispatch({ colorToChange: "red", amount: COLOR_INCCREMENT })
+          dispatch({ type: ACTION_TYPES.CHANGE_RED, payload: COLOR_INCCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: "red", amount: -1 * COLOR_INCCREMENT })
+          dispatch({
+            type: ACTION_TYPES.CHANGE_RED,
+            payload: -1 * COLOR_INCCREMENT,
+          })
         }
       />
       <ColorCounter
         color="Green"
         onIncrease={() =>
-          dispatch({ colorToChange: "green", amount: COLOR_INCCREMENT })
+          dispatch({
+            type: ACTION_TYPES.CHANGE_GREEN,
+            payload: COLOR_INCCREMENT,
+          })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: "green", amount: -1 * COLOR_INCCREMENT })
+          dispatch({
+            type: ACTION_TYPES.CHANGE_GREEN,
+            payload: -1 * COLOR_INCCREMENT,
+          })
         }
       />
       <ColorCounter
         color="Blue"
         onIncrease={() =>
-          dispatch({ colorToChange: "blue", amount: COLOR_INCCREMENT })
+          dispatch({
+            type: ACTION_TYPES.CHANGE_BLUE,
+            payload: COLOR_INCCREMENT,
+          })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: "blue", amount: -1 * COLOR_INCCREMENT })
+          dispatch({
+            type: ACTION_TYPES.CHANGE_BLUE,
+            payload: -1 * COLOR_INCCREMENT,
+          })
         }
       />
 
